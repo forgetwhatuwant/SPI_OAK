28b42e4110e60ecf2985a5d68042b4ba089ee0a4
forgetwhatuwant <2403116352@qq.com>
2024-12-17 17:03:56 +0800

    update

--- upi_python/upi_info_display/scripts/start_system.sh
+++ upi_python/upi_info_display/scripts/start_system.sh
@@ -5,5 +5,5 @@ docker container run --name upi_system -i --rm --privileged --net=host \
     ros:noetic-desktop-full \
     bash -c "
         echo 'Starting ROS launch...'
-        roslaunch /home/codes/raspberry_pi/upi_python/upi_info_display/scripts/d435i.launch
+        roslaunch /home/codes/raspberry_pi/upi_python/upi_info_display/scripts/oak_d_sr.launch
     "&
